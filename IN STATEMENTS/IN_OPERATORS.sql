77c720d921fc8696af21d5f514c43b0c98f3d4bf
Rajan Poudel <raajan.works@gmail.com>
2026-03-06 09:18:48 +0545

    Add files via upload

---
+++ IN STATEMENTS/IN_OPERATORS.sql
@@ -0,0 +1,14 @@
+-- The following SQL statement selects all customers that are located in "VA", "FL" OR "GA":
+SELECT * 
+FROM customers
+WHERE state IN ('VA', 'FL', 'GA');
+
+-- The following SQL statement selects all customers that are NOT located in "VA", "FL" OR "GA":
+SELECT * 
+FROM customers
+WHERE state NOT IN ('VA', 'FL', 'GA');
+
+-- The following SQL statement selects all products that have a quantity_in_stock of 6, 90, 94, or 70
+SELECT * 
+FROM products
+WHERE quantity_in_stock IN (6, 90, 94, 70);
