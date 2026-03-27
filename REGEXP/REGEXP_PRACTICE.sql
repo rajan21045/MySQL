9bca821536a8791de1cbab2767f7088770e41b50
Rajan Poudel <raajan.works@gmail.com>
2026-03-27 20:39:33 +0545

    Add files via upload

---
+++ REGEXP/REGEXP_PRACTICE.sql
@@ -0,0 +1,26 @@
+/*
+Get the customers whose
+	first names are ELKA or AMBUR
+    last names end with EY OR ON
+    last name start with MY or constains SE
+    last names contain B followed by R or U 
+*/
+-- first names are ELKA or AMBUR
+SELECT *
+FROM customers
+WHERE first_name REGEXP 'ELKA|AMBUR';
+
+--     last names end with EY OR ON
+SELECT *
+FROM customers
+WHERE last_name REGEXP '(EY|ON)$';
+
+--     last name start with MY or constains SE
+SELECT *
+FROM customers
+WHERE last_name REGEXP '^MY|SE';
+
+--     last names contain B followed by R or U 
+SELECT *
+FROM customers
+WHERE last_name REGEXP 'B[ru]';
