5d8b4fdccaa67f3e008fb913ec89dbe36ba5d511
Rajan Poudel <raajan.works@gmail.com>
2026-03-28 19:13:33 +0545

    Update REGEXP_PRACTICE.sql

--- REGEXP/REGEXP_PRACTICE.sql
+++ REGEXP/REGEXP_PRACTICE.sql
@@ -11,17 +11,17 @@ SELECT *
 FROM customers
 WHERE first_name REGEXP 'ELKA|AMBUR';
 
---     last names end with EY OR ON
+-- last names end with EY OR ON
 SELECT *
 FROM customers
 WHERE last_name REGEXP '(EY|ON)$';
 
---     last name start with MY or constains SE
+-- last name start with MY or constains SE
 SELECT *
 FROM customers
 WHERE last_name REGEXP '^MY|SE';
 
---     last names contain B followed by R or U 
+-- last names contain B followed by R or U 
 SELECT *
 FROM customers
 WHERE last_name REGEXP 'B[ru]';
