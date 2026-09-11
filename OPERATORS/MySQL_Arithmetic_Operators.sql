edc69d16cc16a1481b0aece7f2ff60aed5452259
Rajan Poudel <raajan.works@gmail.com>
2026-09-11 20:17:15 +0545

    Add files via upload

---
+++ OPERATORS/MySQL_Arithmetic_Operators.sql
@@ -0,0 +1,37 @@
+/*
+SQL Operators
+SQL operators are keywords and symbols used to perform operations with data values.
+SQL operators are used in SQL statements like SELECT, WHERE, LIKE, etc.
+
+SQL operators is categorized into the following types:
+1. Arithmetic operators
+2. Comparison operators
+3. Compound operators
+4. Bitwise operators
+5. Logical operators
+*/
+SELECT 20 + 30 AS TOTAL;
+
+/*
+SQL Arithmetic Operators
+Operator	Description
++			Addition
+-			Subtraction	
+*			Multiplication	
+/			Division	
+%			Modulus
+*/
+-- Addition
+SELECT 20 + 49 AS TOTAL_NUMBER;
+
+-- Subtraction
+SELECT 59 - 49 AS TOTAL_SUBTRACTION_NUMBER;
+
+-- Multiplication
+SELECT 59 * 49 AS TOTAL_MULTIFICATION_NUMBER;
+
+-- DIVISION
+SELECT 59 / 49 AS TOTAL_DIVISION_NUMBER;
+
+-- MODULUS
+SELECT 59 % 49 AS TOTAL_MODULUS_NUMBER;
